1e304db4a6ffb3a3a83162278523e5104e309dc3
Rachele Edwards <dot.com1472@gmail.com>
2020-02-25 20:48:41 -0700

    woot woot Im DONE BABY--goodnight

--- index.js
+++ index.js
@@ -298,9 +298,15 @@ function counterMaker() {
  * counter() // should return 0
  * etc
 */
-function counterMakerWithLimit(/* CODE HERE */) {
-  /* CODE HERE */
-}
+function counterMakerWithLimit(maxValue) {
+  let count = -1;
+  return function counter(){
+    if (count === maxValue){
+      count = -1;    
+     }//if stmt
+      return ++count;
+    }//function
+  }//ending counterMaker
 
 /////////////// END OF CHALLENGE ///////////////
 /////////////// END OF CHALLENGE ///////////////
